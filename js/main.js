02df4965beb85cf116632781ac6b1875cb178daa
Mateus Prates <mateusbprates@hotmail.com>
2021-11-23 15:10:19 -0300

    Tratando a responsividade e colocando umas animações

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0:{
+        items:1
+    },    
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    },
+}
+
 $(document).ready(function () {
 
     $nav = $('.nav');
@@ -14,10 +29,17 @@ $(document).ready(function () {
         autoplayTimeout:3000,
         dots:false,
         nav: true,
-        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next') ]
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next') ],
+        responsive:responsive
     });
 
+    $('.move-up span').click(function(){
+        $('html, body').animate({
+            scrollTop:0
+        }, 1000);
+    });
 
+    AOS.init();
 
 });
 
